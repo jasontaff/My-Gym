4eafc4617b5a0d313b4da46accdb45d09e7b4ecd
jtaff <jtaff@2u.com>
2025-01-13 13:29:31 -0500

    finish adding choices

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -28,8 +28,6 @@ export default function Generator() {
   }
 
   function updateMuscles(muscleGroup){
-
-    
     if(muscles.includes(muscleGroup)){
       setMuscles(muscles.filter(val =>  val !==muscleGroup ))
       return
@@ -47,8 +45,8 @@ export default function Generator() {
 
 
     setMuscles([...muscles, muscleGroup])
-    if(muscles.length === 3){
-
+    if(muscles.length === 2 ){
+      setShowModel(false)
     }
    
   }
@@ -60,6 +58,7 @@ export default function Generator() {
         {Object.keys(WORKOUTS).map((type, typeIndex) => {
           return(
             <button onClick={() =>{
+              setMuscles([])
               setPosion(type)
             }} key={typeIndex} className={'bg-slate-950 border duration-200 hover:border-blue-600 py-3 rounded-lg ' + (type === poison ? 'border-blue-600' : 'border-blue-400')}>
               <p className="capitalize">{type.replaceAll('_', " ")}</p>
@@ -71,18 +70,16 @@ export default function Generator() {
       <Header index={'02'} title={'Lock on targets'} description ={'Select the muscles judged for annihilation'} />
       <div className="bg-slate-950 border border-solid border-blue-400 rounded-lg flex flex-col">
         <button onClick={toggleModel}className="relative py-3 flex items-center justify-center">
-          <p>Select muscle groups</p>
+          <p className="capitalize">{muscles.length == 0 ? 'Select muscle groups' : muscles.join(' ')}</p>
           <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
         </button>
         {showModel && (
           <div className="flex flex-col p-3">
             { (poison === 'individual' ? WORKOUTS[poison] : Object.keys(WORKOUTS[poison])).map((muscleGroup, muscleGroupIndex) => {
               return (
-                <button
-                  key={muscleGroupIndex}
-                  className={`hover:text-blue-400 duration-200 ${muscles.includes(muscleGroup) ? 'text-blue-400' : ''}`}
-                  onClick={() => updateMuscles(muscleGroup)}
-                  aria-label={`Select muscle group ${muscleGroup}`}
+                <button onClick={() => {updateMuscles(muscleGroup)}}
+                 key={muscleGroup}
+                className={'hover:text-blue-400 duration-200' + (muscles.includes(muscleGroup) ? ' text-blue-400' : ' ')}
                 >
                   <p className="uppercase">
                     {muscleGroup.replaceAll('_', '')}
